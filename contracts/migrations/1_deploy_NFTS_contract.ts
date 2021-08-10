8e611b01fec689fd0428427cce941478e149f20b
Steven Jehannet <steven.jehannet@ekino.com>
2021-08-10 18:37:34 +0200

    initialise contract storage

--- contracts/migrations/1_deploy_NFTS_contract.ts
+++ contracts/migrations/1_deploy_NFTS_contract.ts
@@ -1,5 +1,6 @@
 import { TezosToolkit, MichelsonMap } from '@taquito/taquito';
 import { InMemorySigner } from '@taquito/signer';
+import { buf2hex } from '@taquito/utils';
 import saveContractAddress from '../helpers/saveContractAddress';
 import getConfig, { NetworkConfig } from '../config';
 import code from '../build/NFTS_contract.json';
@@ -26,7 +27,27 @@ const deploy = async () => {
             ledger: new MichelsonMap(),
             operators: new MichelsonMap(),
             reverse_ledger: new MichelsonMap(),
-            metadata: new MichelsonMap(),
+            metadata: MichelsonMap.fromLiteral({
+                '': buf2hex(Buffer.from('tezos-storage:contents')),
+                contents: buf2hex(
+                    Buffer.from(
+                        JSON.stringify({
+                            name: 'News Fake Token',
+                            description: 'FA2 NFT Contract News Articles Minter',
+                            version: 'beta',
+                            license: { name: 'MIT' },
+                            authors: 'ekino <blockchain@ekino.com>',
+                            homepage: 'htpps://newsfaketoken.web.app',
+                            source: {
+                                tools: 'cameligo',
+                                location: 'https://github.com/ekino/NewsFakeToken',
+                            },
+                            interfaces: ['TZIP-012', 'TZIP-016'],
+                            permissions: { operator: 'no-transfer' },
+                        }),
+                    ),
+                ),
+            }),
             token_metadata: new MichelsonMap(),
             next_token_id: 0,
             admin: alice.pkh,
